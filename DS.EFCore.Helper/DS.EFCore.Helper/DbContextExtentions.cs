238433f0ee3fc2b9f878752787bc10d3a99c8667
Vitor Luis dos Santos Silva <vitor.silva@fei.edu.br>
2021-12-01 08:43:05 -0300

    Creation os DbContextExtentions

---
+++ DS.EFCore.Helper/DS.EFCore.Helper/DbContextExtentions.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DS.EFCore.Helper
+{
+    public static class DbContextExtentions
+    {
+        public static void RemoveUntrackedEntity<TEntity>(this DbContext dbContext, TEntity entity) where TEntity : class
+        {
+            if (dbContext.Entry(entity).State == EntityState.Detached)
+            {
+                dbContext.Attach(entity);
+            }
+
+            dbContext.Remove(entity);
+        }
+
+        public static void UpdateUntrackedEntity<TEntity>(this DbContext dbContext, TEntity entity) where TEntity : class
+        {
+            if (dbContext.Entry(entity).State == EntityState.Detached)
+            {
+                dbContext.Attach(entity);
+            }
+
+            dbContext.Entry(entity).State = EntityState.Modified;
+        }
+
+        public static void RemoveBy<TEntity>(this DbContext dbContext, Expression<Func<TEntity, bool>> filter) where TEntity : class
+        {
+            TEntity entity = dbContext
+                .Set<TEntity>()
+                .AsTracking()
+                .Single(filter);
+
+            dbContext.Remove(entity);
+        }
+
+        public static async Task RemoveAsyncBy<TEntity>(this DbContext dbContext, Expression<Func<TEntity, bool>> filter) where TEntity : class
+        {
+            TEntity entity = await dbContext
+                .Set<TEntity>()
+                .AsTracking()
+                .SingleAsync(filter);
+
+            dbContext.Remove(entity);
+        }
+
+        public static void RemoveAll<TEntity>(this DbContext dbContext, Expression<Func<TEntity, bool>> filter) where TEntity : class
+        {
+            TEntity[] entities = dbContext
+                .Set<TEntity>()
+                .AsTracking()
+                .Where(filter)
+                .ToArray();
+
+            dbContext.RemoveRange(entities);
+        }
+
+        public static async Task RemoveAllAsync<TEntity>(this DbContext dbContext, Expression<Func<TEntity, bool>> filter) where TEntity : class
+        {
+            TEntity[] entities = await dbContext
+                .Set<TEntity>()
+                .AsTracking()
+                .Where(filter)
+                .ToArrayAsync();
+
+            dbContext.RemoveRange(entities);
+        }
+    }
+}
